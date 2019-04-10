85f30d5bc1813164c9fcadbf34107c95f4c8a856
Krakaw <41575888+Krakaw@users.noreply.github.com>
2019-04-10 13:10:51 +0200

    Cleaned up the build script added prices

--- controllers/build.js
+++ controllers/build.js
@@ -1,35 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const fs = require("fs");
+
+/** Server types */
+const TYPE_SLOW = "slow";
+const TYPE_FAST = "fast";
+const TYPE_FASTER = "faster";
+const TYPE_FASTEST = "fastest";
+
+/** Environment variables */
 const DEBUG = process.env.DEBUG;
+const BUILD_KEY = process.env.BUILD_KEY;
+const AUTHORIZED_USERS = (process.env.BUILD_AUTHORIZED_USERS || "").split(",");
+//Default to 15 minutes
+const TIME_BETWEEN_MODIFICATIONS = (process.env.BUILD_TIME_BETWEEN_MODIFICATION || 15) * 60 * 1000;
+//Where we place the output from the script
+const BUILD_OUTPUT_FILE = process.env.BUILD_OUTPUT_FILE;
+//Where we read the current server size from
+const BUILD_CURRENT_FILE = process.env.BUILD_CURRENT_FILE;
 
 router.post('/', (req, res) => {
 
     if (DEBUG) {
         console.log(req.body);
-    }
-    const BUILD_KEY = process.env.BUILD_KEY;
-    const AUTHORIZED_USERS = process.env.BUILD_AUTHORIZED_USERS.split(",");
-    if (DEBUG) {
         console.log("Authorized Users:", AUTHORIZED_USERS);
     }
-    //Default to 15 minutes
-    const TIME_BETWEEN_MODIFICATIONS = (process.env.BUILD_TIME_BETWEEN_MODIFICATION || 15) * 60 * 1000;
-    const BUILD_OUTPUT_FILE = process.env.BUILD_OUTPUT_FILE;
-    const BUILD_CURRENT_FILE = process.env.BUILD_CURRENT_FILE;
 
     const {body: {token = null, user_name = null, text = ""}} = req;
 
-    let requestedServer = text.trim().toLowerCase();
-    let currentSize = "Unknown";
-    if (fs.existsSync(BUILD_CURRENT_FILE)) {
-        currentSize = fs.readFileSync(BUILD_CURRENT_FILE, 'utf8');
-    }
-    if (!requestedServer) {
-        //This is just polling for the current size
-        return respond(res, `The current drone build server size is \`${currentSize}\``);
+    let requestedServerType = text.trim().toLowerCase();
+    let currentName = getCurrentServerName();
+    let currentType = getTypeFromName(currentName);
+    let currentDescription = getCompleteDescription(currentType);
+
+    //This is just polling for the current size
+    if (!requestedServerType) {
+        return respond(res, `Current Drone is: \`${currentDescription}\``);
     }
 
+    const queryIsValid = validateUserCanChange(res, user_name, token);
+    if (queryIsValid === true) {
+        let responseText = "";
+        if (types(requestedServerType)) {
+            //It is a valid query, change the server size
+            setServerSize(requestedServerType);
+            responseText =  `Old Size: \`${currentName}\`\n${getCompleteDescription(requestedServerType)}`;
+        } else {
+            responseText = `Current Size: \`${currentName}\`\nInvalid input \`${requestedServerType}\`: /build [${Object.keys(types).join("|")}]`;
+        }
+        return respond(res, responseText);
+    } else {
+        return queryIsValid;
+    }
+});
+
+function validateUserCanChange(res, user_name, token) {
     //Only the authorized users can execute this function
     if (AUTHORIZED_USERS.indexOf(user_name) === -1) {
         return respond(res, "Unauthorized user");
@@ -41,7 +66,6 @@ router.post('/', (req, res) => {
     }
 
     //Make sure that the file we are editing exists
-
     if (!fs.existsSync(BUILD_OUTPUT_FILE)) {
         return respond(res, "Invalid build output file path");
     }
@@ -58,26 +82,82 @@ router.post('/', (req, res) => {
         return respond(res, `The file can only be modified again in ${Math.round((TIME_BETWEEN_MODIFICATIONS - timeSinceLastModification) / 1000 / 60)} minutes`)
     }
 
-    const types = {
-        'slow': 'c5.xlarge',
-        'fast': 'c5.2xlarge',
-        'faster': 'c5.4xlarge',
-        'fastest': 'c5.9xlarge',
+    return true;
+}
+
+function setServerSize(newSize) {
+    fs.writeFileSync(BUILD_OUTPUT_FILE, newSize, {encoding: 'utf8', flag: 'w'});
+}
+
+function getCurrentServerName() {
+    let currentSize = "Unknown";
+    if (fs.existsSync(BUILD_CURRENT_FILE)) {
+        currentSize = fs.readFileSync(BUILD_CURRENT_FILE, 'utf8').trim();
+        currentSize = currentSize.split(" ")[4];
+    }
+    return currentSize;
+}
+
+function getTypeFromName(name) {
+    name = name.trim();
+    let TYPES = types();
+    for (let type in TYPES) {
+        let typeName = TYPES[type];
+        if (typeName === name) {
+            return type;
+        }
+    }
+    return null;
+}
+
+function getCompleteDescription(type) {
+    let description = descriptions(type);
+    if (description) {
+        description = `${description} - \$${prices(type)}/h - \$${prices(type) * 720}/month`;
+    }
+    return description;
+}
+
+function types(type) {
+    const TYPES = {
+        [TYPE_SLOW]: TYPE_SLOW,
+        [TYPE_FAST]: TYPE_FAST,
+        [TYPE_FASTER]: TYPE_FASTER,
+        [TYPE_FASTEST]: TYPE_FASTEST,
+    };
+    if (type) {
+        return TYPES[type];
+    }
+    return TYPES;
+}
+
+function descriptions(type) {
+    const NAMES = {
+        [TYPE_SLOW]: `${TYPE_SLOW} - c5.xlarge`,
+        [TYPE_FAST]: `${TYPE_FAST} - c5.2xlarge`,
+        [TYPE_FASTER]: `${TYPE_FASTER} - c5.4xlarge`,
+        [TYPE_FASTEST]: `${TYPE_FASTEST} - c5.9xlarge`,
     };
 
-    let serverDescription = checkServerSize(types, requestedServer);
+    if (type) {
+        return NAMES[type];
+    }
+    return NAMES;
+}
 
-    let responseText = `Current Size: \`${currentSize}\`\nInvalid input \`${requestedServer}\`: /build [${Object.keys(types).join("|")}]`;
-
-    if (serverDescription) {
-        fs.writeFileSync(BUILD_OUTPUT_FILE, requestedServer, {encoding: 'utf8', flag: 'w'});
-        responseText = `Old Size: \`${currentSize}\`\nThe build server has been set to ${requestedServer} - ${types[requestedServer]}`;
-        return respond(res, responseText);
-    } else {
-        return respond(res, responseText);
+function prices(type) {
+    const PRICES = {
+        [TYPE_SLOW]: 0.17,
+        [TYPE_FAST]: 0.34,
+        [TYPE_FASTER]: 0.68,
+        [TYPE_FASTEST]: 1.53,
     }
 
-});
+    if (type) {
+        return PRICES[type];
+    }
+    return PRICES;
+}
 
 function respond(res, message) {
     if (DEBUG) {
@@ -86,9 +166,9 @@ function respond(res, message) {
     return res.send(message.trim());
 }
 
-function checkServerSize(types, requestedType) {
+function checkServerSize(requestedType) {
     requestedType = requestedType.toLowerCase();
-    return types[requestedType];
+    return types(requestedType);
 }
 
 
