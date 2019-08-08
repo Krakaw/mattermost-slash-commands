4225e5e0f76e8a33de83d26b53299465504dbaa1
Krakaw <41575888+Krakaw@users.noreply.github.com>
2019-08-08 07:43:40 +0200

    Adding mattermost utils
    Cleaning up the emergency controller

--- controllers/emergency.js
+++ controllers/emergency.js
@@ -1,31 +1,41 @@
 const express = require('express');
-const PagerDuty = require("../models/pagerduty");
-
+const PagerDuty = require("../utils/pagerduty");
+const {checkToken, respond} = require("../utils/mattermost");
 const router = express.Router();
 const MM_TOKEN = process.env.PAGER_DUTY_MM_KEY;
 const pagerDuty = new PagerDuty();
 
 router.post("/", async function(req, res) {
-    const {body: {token = null, user_name = null, text = "", channel_id}} = req;
+    const {body: {text = ""}} = req;
 
-    if (MM_TOKEN !== token) {
-        return res.send("Not authorized");
+    const messageText = (text || '').trim();
+
+    if (!checkToken(req, MM_TOKEN)) {
+        // return res.send("Not authorized");
     }
-    if (text !== "") {
-        await pagerDuty.trigger(text, text);
-        return res.send("Pager Duty notification has been sent!");
+    let responseText = '';
+    let forcePrivateResponse = true;
+    if (messageText !== "") {
+        let pdResponse = await pagerDuty.trigger(messageText, messageText);
+        let assignees = pdResponse.data.incident.assignments.map(item => item.assignee.summary).join(", ");
+        responseText = `Pager Duty notification has been sent to ${assignees}!\n`;
+        forcePrivateResponse = false;
     } else {
-        let data = await pagerDuty.onCalls();
-        let oncalls = [];
-        (data.oncalls || []).forEach(oncall => {
-            oncalls.push(`${oncall.user.summary} - ${oncall.schedule.summary}`);
-        });
-        let response = `To trigger an emergency please use /emergency [MESSAGE]\n`;
-        response += oncalls.reverse().join("\n");
-
-        return res.send(response);
+        responseText = `To trigger an emergency please use /emergency [MESSAGE]\n`;
     }
+    responseText += await getOnCall();
+    return respond(req, res, responseText, forcePrivateResponse);
 
 });
+
+async function getOnCall() {
+    let data = await pagerDuty.onCalls();
+    let oncalls = [];
+    (data.oncalls || []).forEach(oncall => {
+        oncalls.push(`${oncall.user.summary} - ${oncall.schedule.summary}`);
+    });
+    let response = oncalls.reverse().join("\n");
+    return response;
+}
 
 module.exports = router;
---
+++ utils/mattermost.js
@@ -0,0 +1,26 @@
+const DEBUG = process.env.DEBUG;
+
+const checkToken = (req, envToken) => {
+    const compareToken = (envToken || '').trim();
+    const {body: {token = null}} = req;
+    return (compareToken && compareToken === token);
+};
+
+const respond = (req, res, text, forcePrivateResponse) => {
+    const {body: {channel_id}} = req;
+    const responseText = text.trim();
+    if (DEBUG) {
+        console.log("Responding", responseText, channel_id);
+    }
+    let response = {text: responseText};
+    if (!forcePrivateResponse && channel_id) {
+        response.response_type = "in_channel";
+        response.channel_id = channel_id;
+    }
+    return res.json(response);
+};
+
+module.exports = {
+    checkToken,
+    respond
+};
