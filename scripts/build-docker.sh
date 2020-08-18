89d6434a92f1af652fbdfa61e8fafab53abba36c
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-08-18 12:46:44 +0200

    Update build-docker for non auth access

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -6,7 +6,7 @@ DOCKER_BUILDKIT=1 docker build -t "$IMAGE_NAME:$TAG" --progress=plain .
 
 if [ -n "$SSH_HOST" ]; then
   echo "Deploying to $SSH_HOST"
-  docker save "$IMAGE_NAME:$TAG" | bzip2 | pv | ssh -o 'RemoteCommand=none' "$SSH_HOST"  'bunzip2 | docker load'
+  docker save "$IMAGE_NAME:$TAG" | bzip2 | pv | ssh -o 'RemoteCommand=none' "$SSH_HOST"  'bunzip2 > docker_image.tar'
 else
   echo 'Set $SSH_HOST to automatically deploy'
 fi
