27f8c8d9ff7e39ae0c6a7fd7480ab9ab54e596a6
Krakaw <41575888+Krakaw@users.noreply.github.com>
2021-12-30 08:29:46 +0200

    Initial release proxy

---
+++ controllers/release.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.post("/", async function(req, res) {
+    // List notes for env + version
+    // Add note for env + version
+    // Complete notes for env + version
+});
+module.exports = router;
